a74bc82b64573dd1e3e54ef4ec3ae7fa77619d6f
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2020-09-29 16:07:12 +0400

    feat: implements fuctions min,max,avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || arguments.length === 0 || !Array.isArray(array) || array.length === 0) return 0;
+  let res = Math.min(...array);
+  if (isNaN(res)) return 0;
+  return res;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || arguments.length === 0 || !Array.isArray(array) || array.length === 0) return 0;
+  let res = Math.max(...array);
+  if (isNaN(res)) return 0;
+  return res;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || arguments.length === 0 || !Array.isArray(array) || array.length === 0) return 0;
+  let res = array.reduce((acc,cur) => acc + cur,0) / array.length;
+  if (isNaN(res)) return 0;
+  return res;
 }
